c966ff9825fdc70faecb6904c97b42509b20b921
MarcoV <marco.verzeni@mail.polimi.it>
2021-11-10 23:31:36 +0100

    initial commit fork with favorites

---
+++ index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+const notifier = require("./lib/notifier");
+const { pollFavoriteBusinesses$ } = require("./lib/poller");
+const { editConfig, resetConfig, configPath, config } = require("./lib/config");
+
+const argv = require("yargs")
+  .usage("Usage: toogoodtogo-watcher <command>")
+  .env("TOOGOODTOGO")
+  .command("config", "Edit the config file.")
+  .command("config-reset", "Reset the config to the default values.")
+  .command("config-path", "Show the path of the config file.")
+  .command("watch", "Watch your favourite businesses for changes.", {
+    config: {
+      type: "string",
+      describe:
+        "Custom config. Note: the config will be overwrite the current config file.",
+    },
+  })
+  .demandCommand().argv;
+
+switch (argv._[0]) {
+  case "config":
+    editConfig();
+    break;
+
+  case "config-reset":
+    resetConfig();
+    break;
+
+  case "config-path":
+    configPath();
+    break;
+
+  case "watch":
+    if (argv.config) {
+      const customConfig = JSON.parse(argv.config);
+      config.set(customConfig);
+    }
+
+    pollFavoriteBusinesses$(notifier.hasListeners$()).subscribe(
+      (businesses) => notifier.notifyIfChanged(businesses),
+      console.error
+    );
+    break;
+}
